56c6e3f3408c154175811b16340ec00987b1b635
jeina7 <jane1231001@gmail.com>
2019-11-19 17:12:32 +0900

    [update] NaN data preprocessing

--- Crawling/textcrawler/pipelines.py
+++ Crawling/textcrawler/pipelines.py
@@ -12,14 +12,15 @@ from scrapy.exceptions import DropItem
 
 class TextcrawlerPipeline(object):
     def __init__(self):
-        self.file = open("textCrawl.csv", 'wb')
+        file_name = "storyCrawl.csv"
+        self.file = open(file_name, 'wb')
         self.exporter = CsvItemExporter(self.file, encoding='utf-8')
         self.exporter.start_exporting()
- 
+
     def close_spider(self, spider):
         self.exporter.finish_exporting()
         self.file.close()
- 
+
     def process_item(self, item, spider):
         self.exporter.export_item(item)
         return item
