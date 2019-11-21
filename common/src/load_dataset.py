442c1291dd5a96830306a77e47ab0405d51c2120
jeina7 <jane1231001@gmail.com>
2019-11-21 16:43:48 +0900

    [update] Sampler_crawled_data

--- common/src/load_dataset.py
+++ common/src/load_dataset.py
@@ -10,16 +10,16 @@ class Sampler_hdf5_news_cut(object):
 
         self.group = group
         self.dataset = []
-        category_count = []
+        self.category_count = []
 
         for int_name in range(len(group)):
             name_dataset = str(int_name)
             self.dataset.append(group[name_dataset])
-            category_count.append(len(group[name_dataset]))
+            self.category_count.append(len(group[name_dataset]))
 
         self.rs = np.random.RandomState(seed=seed)
-        category_count = np.array(category_count)
-        self.category_prob = list(category_count/sum(category_count))
+        self.category_count = np.array(self.category_count)
+        self.category_prob = list(self.category_count/sum(self.category_count))
 
     def sample(self, length):
         selected_category = np.random.choice(len(self.group), 1, self.category_prob)[0]
@@ -27,3 +27,18 @@ class Sampler_hdf5_news_cut(object):
 
         index = self.rs.randint(0, len(self.dataset[selected_category][news_selected_category]) - length + 1)
         return self.dataset[selected_category][news_selected_category][index:length+index]
+
+
+class Sampler_crawled_data(object):
+    def __init__(self, group, seed=None):
+        self.group = group
+        self.dataset = []
+
+        for key in group.keys():
+            self.dataset.append(group[key])
+
+        self.rs = np.random.RandomState(seed=seed)
+
+    def sample(self, length):
+        index = self.rs.randint(0, len(self.dataset[0]) - length + 1)
+        return self.dataset[0][index:length+index]
